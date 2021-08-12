7820cdd4d53bb8b261c22f855a29dfc54b10982e
celiloz <celiloz45@hotmail.com>
2021-08-12 14:13:18 +0300

    Account

---
+++ controllers/account.js
@@ -0,0 +1,28 @@
+exports.getLogin = (req, res, next) => {
+    res.render('account/login', {
+        path: '/login',
+        title: 'Login'
+    })
+}
+exports.postLogin = (req, res, next) => {
+    res.redirect('/');
+    
+}
+exports.getRegister = (req, res, next) => {
+    res.render('account/register', {
+        path: '/register',
+        title: 'Register'
+    })
+}
+exports.postRegister = (req, res, next) => {
+    res.redirect('/')
+}
+exports.getRegister = (req, res, next) => {
+    res.render('account/reset', {
+        path: '/reset',
+        title: 'Reset'
+    })
+}
+exports.postRegister = (req, res, next) => {
+    res.redirect('/login')
+}
